aff02ce0b8b2c0171c337a159645e17063912a29
athunlal <athunlalp@gmail.com>
2022-12-20 19:56:19 +0530

    Read file updated

--- Reade/main.go
+++ Reade/main.go
@@ -7,24 +7,39 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type Users struct {
+	First_name string `json:"first_name"`
+	Last_name  string `json:"last_name"`
+	Email      string `json:"email"`
+	Gender     string `json:"gender"`
+}
+
 func main() {
 	fmt.Println("Trying to connect....")
 
-	
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/test1")
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/company")
 	if err != nil {
 		panic(err.Error())
 	}
 
 	defer db.Close()
 
-	insertion, err := db.Query("INSERT INTO users VALUES('aTHUNL lal')")
+	Read, err := db.Query("SELECT first_name, last_name, email, gender from test1")
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	defer insertion.Close()
+	for Read.Next() {
+		var user Users
 
-	fmt.Println("Succesfully Connected into users ")
+		err = Read.Scan(&user.First_name, &user.Last_name, &user.Email, &user.Gender)
+
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(user.First_name, user.Last_name, user.Email, user.Gender)
+	}
+
+	fmt.Println("Succesfully  Read Users ")
 }
